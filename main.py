adc6c039cd62e103b1037797f959b5564cc59b18
Bohdan Tron <bohdan.tron@gmail.com>
2025-05-19 15:04:39 +0300

    feat: added cache & missing data

--- main.py
+++ main.py
@@ -7,15 +7,35 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path='envs/.env')
 
 
-api_key = os.environ.get('NASA_API_KEY')
-image_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-r = req.get(image_url)
-data = r.json()
+@st.cache_resource
+def get_nasa_image():
+  print('its uncached')
+  api_key = os.environ.get('NASA_API_KEY')
+  image_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+  r = req.get(image_url)
+  data = r.json()
+  return data
+
+
+data = get_nasa_image()
 
 
 st.title("Space pic of the day")
+
 st.header(data['title'])
-st.image(data['url'])
-st.write(data['copyright'])
-st.write(data['explanation'])
+
+try:
+  st.image(data['url'])
+except:
+  st.write("No image found")
+
+try:
+  st.write(data['copyright'])
+except:
+  st.write("No copyright found")
+
+try:
+  st.write(data['explanation'])
+except:
+  st.write("No explanation found")
 
