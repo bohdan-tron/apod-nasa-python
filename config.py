93606a9ab3d047cb00ba55346829ac0b63cabd46
Bohdan Tron <bohdan.tron@gmail.com>
2025-05-19 15:29:41 +0300

    feat: date picker added, overall improvements implemented

---
+++ config.py
@@ -0,0 +1,2 @@
+NASA_API_ENDPOINT = "https://api.nasa.gov/planetary/apod"
+ENV_FILE_PATH = 'envs/.env'
--- main.py
+++ main.py
@@ -2,25 +2,36 @@ import streamlit as st
 import requests as req
 import os
 from dotenv import load_dotenv
+from config import NASA_API_ENDPOINT, ENV_FILE_PATH
+from typing import Optional, Dict, Any
 
 
-load_dotenv(dotenv_path='envs/.env')
+load_dotenv(dotenv_path=ENV_FILE_PATH)
+api_key = os.environ.get('NASA_API_KEY')
+api_query = f"{NASA_API_ENDPOINT}?api_key={api_key}"
+
+
+selected_date = st.date_input("Select date")
+api_query = f"{NASA_API_ENDPOINT}?api_key={api_key}&date={selected_date}"
 
 
 @st.cache_resource
-def get_nasa_image():
-  print('its uncached')
-  api_key = os.environ.get('NASA_API_KEY')
-  image_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-  r = req.get(image_url)
+def get_nasa_image(url_to_load) -> Optional[Dict[str, Any]]:
+  r = req.get(url_to_load, timeout=10)
   data = r.json()
   return data
 
 
-data = get_nasa_image()
+with st.spinner('Fetching data from NASA API...'):
+  st.cache_resource.clear()
+  data: Dict[str, Any] | None = get_nasa_image(api_query)
 
 
 st.title("Space pic of the day")
+
+if data is None:
+  st.write("Error getting data from NASA API")
+  exit()
 
 st.header(data['title'])
 
@@ -38,4 +49,3 @@ try:
   st.write(data['explanation'])
 except:
   st.write("No explanation found")
-
